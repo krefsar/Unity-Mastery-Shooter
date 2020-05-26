e0dd52953ef208ebadd7f6039c530b95185a7982
Evan Martinez <evanm.design@gmail.com>
2020-05-26 08:58:17 -0400

    character walk run with camera follow

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController characterController;
+
+    [SerializeField]
+    private float rotationSpeed = 30f;
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Speed", vertical);
+
+        transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
+
+        characterController.SimpleMove(transform.forward * vertical * moveSpeed * Time.deltaTime);
+    }
+}
