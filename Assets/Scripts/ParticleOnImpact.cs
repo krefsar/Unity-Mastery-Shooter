642e0d347433f80a67e91876b475aaba1a491a87
Evan Martinez <evanm.design@gmail.com>
2020-05-29 11:38:48 -0400

    laser impact effect

---
+++ Assets/Scripts/ParticleOnImpact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ParticleOnImpact : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject particlePrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(particlePrefab, transform.position, Quaternion.LookRotation(collision.contacts[0].normal));
+        Destroy(gameObject);
+    }
+}
