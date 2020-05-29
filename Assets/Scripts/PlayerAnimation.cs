12066dbd4941dfe15c1604655b07a60cab1f29b1
Evan Martinez <evanm.design@gmail.com>
2020-05-28 20:41:12 -0400

    add player animation blend script

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float drawWeaponSpeed;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(FadeToShootingLayer());
+        } else if (Input.GetButtonUp("Fire1"))
+        {
+            StartCoroutine(FadeFromShootingLayer());
+        }
+    }
+
+    private IEnumerator FadeToShootingLayer()
+    {
+        float currentWeight = animator.GetLayerWeight(1);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < drawWeaponSpeed)
+        {
+            elapsedTime += Time.deltaTime;
+
+            currentWeight += Time.deltaTime / drawWeaponSpeed;
+            animator.SetLayerWeight(1, currentWeight);
+
+            yield return null;
+        }
+
+        animator.SetLayerWeight(1, 1);
+    }
+
+    private IEnumerator FadeFromShootingLayer()
+    {
+        float currentWeight = animator.GetLayerWeight(1);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < drawWeaponSpeed)
+        {
+            elapsedTime += Time.deltaTime;
+
+            currentWeight -= Time.deltaTime / drawWeaponSpeed;
+            animator.SetLayerWeight(1, currentWeight);
+
+            yield return null;
+        }
+
+        animator.SetLayerWeight(1, 0);
+    }
+}
