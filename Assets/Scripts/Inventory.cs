aada5370b665705fba6636cd11b7f5929a0a4e97
Evan Martinez <evanm.design@gmail.com>
2020-05-28 20:49:19 -0400

    create weapon inventory and switching

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private Weapon[] weapons;
+
+    private void Update()
+    {
+        foreach (var weapon in weapons)
+        {
+            if (Input.GetKeyDown(weapon.WeaponHotkey))
+            {
+                SwitchToWeapon(weapon);
+                break;
+            }
+        }
+    }
+
+    private void SwitchToWeapon(Weapon weaponToUse)
+    {
+        foreach (var weapon in weapons)
+        {
+            weapon.gameObject.SetActive(weapon == weaponToUse);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public KeyCode WeaponHotkey {  get { return weaponHotKey; } }
+
+    [SerializeField]
+    private KeyCode weaponHotKey;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (CanFire())
+            {
+                Fire();
+            }
+        }
+    }
+
+    private void Fire()
+    {
+        Debug.Log("Firing weapon " + gameObject.name);
+    }
+
+    private bool CanFire()
+    {
+        return true;
+    }
+}
