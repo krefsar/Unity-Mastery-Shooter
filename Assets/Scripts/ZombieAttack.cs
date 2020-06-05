9e299c8896f64e11ad77400e49ddba24d53d54e8
Evan Martinez <evanm.design@gmail.com>
2020-06-05 09:41:57 -0400

    start adding zombie attack and player health

---
+++ Assets/Scripts/ZombieAttack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAttack : MonoBehaviour
+{
+    public event Action OnAttack = delegate { };
+
+    [SerializeField]
+    [Tooltip("How long the zombie waits after an attack to attack again")]
+    private float delayBetweenAttacks = 1.5f;
+    [SerializeField]
+    private float delayBetweenAnimationAndDamage = 0.5f;
+    [SerializeField]
+    [Tooltip("How far away the zombie can attack from")]
+    private float maximumAttackRange = 1.5f;
+    [SerializeField]
+    private int damage = 1;
+
+    private Health playerHealth;
+    private float attackTimer;
+
+    private void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerMovement>().GetComponent<Health>();
+    }
+
+    private void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (CanAttack())
+        {
+            attackTimer = 0;
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        OnAttack();
+        StartCoroutine(DealDamageAfterDelay());
+    }
+
+    private IEnumerator DealDamageAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBetweenAnimationAndDamage);
+        playerHealth.TakeHit(damage);
+    }
+
+    private bool CanAttack()
+    {
+        return attackTimer >= delayBetweenAttacks &&
+            Vector3.Distance(transform.position, playerHealth.transform.position) <= maximumAttackRange;
+    }
+}
