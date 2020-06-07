0e801c8626740c680fea9aef2139b7ab3818c891
Evan Martinez <evanm.design@gmail.com>
2020-06-07 15:52:10 -0400

    hook in ranged floats for grunts

--- Assets/Scripts/Audio/SimpleAudioEvent.cs
+++ Assets/Scripts/Audio/SimpleAudioEvent.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(menuName = "Audio Events/Simple")]
 public class SimpleAudioEvent : ScriptableObject
@@ -10,11 +11,25 @@ public class SimpleAudioEvent : ScriptableObject
     [SerializeField]
     [MinMaxRange(0f, 2f)]
     private RangedFloat pitch = new RangedFloat(1, 1);
+    [SerializeField]
+    [MinMaxRange(0f, 1000f)]
+    private RangedFloat distance = new RangedFloat(1, 1000);
+    [SerializeField]
+    private AudioMixerGroup mixer;
 
     public void Play(AudioSource source)
     {
+        source.outputAudioMixerGroup = mixer;
+
         int clipIndex = Random.Range(0, clips.Length);
         source.clip = clips[clipIndex];
+
+        source.pitch = Random.Range(pitch.minValue, pitch.maxValue);
+        source.volume = Random.Range(volume.minValue, volume.maxValue);
+
+        source.minDistance = distance.minValue;
+        source.maxDistance = distance.maxValue;
+
         source.Play();
     }
 }
