5c07dc528718c18ed251f5ab1b05e08bd5263339
Evan Martinez <evanm.design@gmail.com>
2020-06-07 15:57:35 -0400

    add zombie grunts

---
+++ Assets/Scripts/ImpactAudio.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Health))]
+public class ImpactAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private SimpleAudioEvent audioEvent;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        GetComponent<Health>().OnTookHit += HandleTookHit;
+    }
+
+    private void HandleTookHit()
+    {
+        audioEvent.Play(audioSource);
+    }
+}
