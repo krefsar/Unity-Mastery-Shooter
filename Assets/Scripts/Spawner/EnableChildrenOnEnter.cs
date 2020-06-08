b0a555aa0b65a811b0dc7737c999c3b3c3c5ed83
Evan Martinez <evanm.design@gmail.com>
2020-06-07 21:25:53 -0400

    spawn enablers

---
+++ Assets/Scripts/Spawner/EnableChildrenOnEnter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnableChildrenOnEnter : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 15f;
+
+    private void Awake()
+    {
+        bool state = false;
+        ToggleChildren(state);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            ToggleChildren(true);
+        }
+    }
+
+    private void ToggleChildren(bool state)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(state);
+        }
+    }
+
+    private void OnValidate()
+    {
+        var collider = GetComponent<Collider>();
+        if (collider == null)
+        {
+            collider = gameObject.AddComponent<SphereCollider>();
+            ((SphereCollider)collider).radius = radius;
+        }
+
+        collider.isTrigger = true;
+    }
+}
