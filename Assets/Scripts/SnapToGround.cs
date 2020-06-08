9b86e790c72dc246be5a2ef06def538d26117da0
Evan Martinez <evanm.design@gmail.com>
2020-06-07 21:14:20 -0400

    make spawners snap to ground

---
+++ Assets/Scripts/SnapToGround.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapToGround : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask layerMask;
+
+    private void Update()
+    {
+        if (layerMask == 0)
+        {
+            layerMask = LayerMask.GetMask("Default");
+        }
+
+        transform.rotation = Quaternion.identity;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, 2f, layerMask))
+        {
+            transform.position = new Vector3(transform.position.x, hitInfo.point.y, transform.position.z);
+        }
+    }
+}
+#endif
